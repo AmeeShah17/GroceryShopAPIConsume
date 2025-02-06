8ed785a106438d19b9156e8528e012686becf848
amee shah <ameedshah17@gmail.com>
2025-02-06 22:40:39 +0530

    fifth commit

---
+++ GroceryShopAPIConsume/Controllers/User/UserCartController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GroceryShopAPIConsume.Controllers.User
+{
+    public class UserCartController : Controller
+    {
+        public IActionResult UserCartDisplay()
+        {
+            return View();
+        }
+    }
+}
--- GroceryShopAPIConsume/Controllers/User/UserProductController.cs
+++ GroceryShopAPIConsume/Controllers/User/UserProductController.cs
@@ -1,12 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GroceryShopAPIConsume.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace GroceryShopAPIConsume.Controllers.User
 {
     public class UserProductController : Controller
     {
-        public IActionResult UserProductDisplay()
+        Uri baseAddress = new Uri("https://localhost:7011/api");
+        private readonly HttpClient _client;
+
+        public UserProductController()
         {
-            return View();
+            _client = new HttpClient();
+            _client.BaseAddress = baseAddress; ;
+        }
+        [HttpGet]
+        public async Task<IActionResult> UserProductDisplay()
+        {
+            List<ProductModel> products = new List<ProductModel>();
+
+            // Fetch product list
+            var response = await _client.GetAsync($"{_client.BaseAddress}/Product/GetAll");
+            if (response.IsSuccessStatusCode)
+            {
+                var data = await response.Content.ReadAsStringAsync();
+                products = JsonConvert.DeserializeObject<List<ProductModel>>(data);
+            }
+            return View(products);
         }
     }
 }
