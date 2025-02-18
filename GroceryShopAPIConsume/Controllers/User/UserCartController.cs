8ac82711b65f68388fec433dd0d41658d800f794
amee shah <ameedshah17@gmail.com>
2025-02-18 23:35:44 +0530

    seventh commit

--- GroceryShopAPIConsume/Controllers/User/UserCartController.cs
+++ GroceryShopAPIConsume/Controllers/User/UserCartController.cs
@@ -1,11 +1,62 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GroceryShopAPIConsume.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Security.Claims;
+using System.Text;
 
 namespace GroceryShopAPIConsume.Controllers.User
 {
     public class UserCartController : Controller
     {
+        Uri baseAddress = new Uri("https://localhost:7011/api");
+        private readonly HttpClient _client;
+
+        public UserCartController()
+        {
+            _client = new HttpClient();
+            _client.BaseAddress = baseAddress; ;
+        }
         public IActionResult UserCartDisplay()
         {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Save([FromForm] OrderModel order)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var json = JsonConvert.SerializeObject(order);
+                    var content = new StringContent(json, Encoding.UTF8, "application/json");
+                    HttpResponseMessage response;
+                    
+
+                    if (order.OrderID == null || order.OrderID == 0)
+                    {
+                        response = await _client.PostAsync($"{_client.BaseAddress}/Order/Add", content);
+                        if (response.IsSuccessStatusCode)
+                        {
+                            //TempData["Message"] = "Record Inserted Successfully";
+                            return RedirectToAction("Index","UserHome");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                TempData["ErrorMessage"] = ex.Message;
+                Console.WriteLine(TempData["ErrorMessage"]);
+            }
+            //await LoadCustomerList();
+            return RedirectToAction("UserProductDisplay","UserProduct");
+        }
+
+        public IActionResult AddOrderForm()
+        {
+            var token = HttpContext.Session.GetString("JWTToken"); // Retrieve the token from session
+            ViewBag.Token = token; // Pass the token to the view
             return View();
         }
     }
--- GroceryShopAPIConsume/Controllers/User/UserHomeController.cs
+++ GroceryShopAPIConsume/Controllers/User/UserHomeController.cs
@@ -118,5 +118,6 @@ namespace GroceryShopAPIConsume.Controllers.User
             }
         }
 
+
     }
 }
